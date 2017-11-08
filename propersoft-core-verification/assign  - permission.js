97bfa3b10db732d1b16a7ffeaf4b4ade052929fc
feel1884257 <616023810@qq.com>
2017-11-08 14:04:48 +0800

    增加两个rules

---
+++ propersoft-core-verification/assign  - permission.js
@@ -0,0 +1,27 @@
+/**
+ * This is a template for an on-change rule. This rule defines what
+ * happens when a change is applied to an issue.
+ *
+ * For details, read the Quick Start Guide:
+ * https://www.jetbrains.com/help/youtrack/standalone/2017.2/Quick-Start-Guide-Workflows-JS.html
+ */
+
+var entities = require('v1/entities');
+var workflow = require('v1/workflow');
+
+exports.rule = entities.Issue.onChange({
+    title: 'Unassignee仅项目负责人和任务创建人可调整',
+    action: function(ctx) {
+        var issue = ctx.issue;
+        var project = issue.project;
+        if (issue.isChanged('Assignee')) {
+            var hasPermission = false;
+            var currentUser = entities.User.current.login;
+            hasPermission = currentUser === issue.reporter.login || currentUser === project.leader.login;
+            workflow.check(hasPermission, 'Unassignee仅项目负责人 ' + project.leader.fullName + ' 和任务创建人 ' + issue.reporter.fullName + ' 可调整!');
+        }
+    },
+  requirements: {
+    // TODO: add requirements
+  }
+});
