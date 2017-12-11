20a521c647de9c69f35ba2f652597dc7bcb76278
AlphaHinex <AlphaHinex@gmail.com>
2017-12-11 17:24:53 +0800

    Add rule

--- propersoft-core-verification/auto-raise-priority.js
+++ propersoft-core-verification/auto-raise-priority.js
@@ -10,13 +10,17 @@ var entities = require('v1/entities');
 
 exports.rule = entities.Issue.onSchedule({
   title: '优先级自动升级',
-  search: '#Unresolved #Major #Critical #Show-stopper ',
-  cron: '0 0 2 * * ?',
+  search: '#Unresolved #Major #Critical #Show-stopper',
+  // From Tuesday to Saturday
+  // http://www.quartz-scheduler.org/documentation/quartz-2.x/tutorials/crontrigger.html
+  cron: '0 0 2 ? * TUE-SAT',
   action: function(ctx) {
     var issue = ctx.issue;
+    console.log('Start to raise priority of ' + issue.id);
     var major = issue.project.findFieldByName('Priority').findValueByName('Major');
     var critical = issue.project.findFieldByName('Priority').findValueByName('Critical');
     var ss = issue.project.findFieldByName('Priority').findValueByName('Show-stopper');
+    
     if (issue.Priority.name === 'Major') {
       issue.Priority = critical;
     } else if (issue.Priority.name === 'Critical') {
@@ -25,6 +29,7 @@ exports.rule = entities.Issue.onSchedule({
       var dd = issue.Delays || 0;
       issue.Delays = dd + 1;
     }
+    console.log('Complete raise priority of ' + issue.id);
   },
   requirements: {
     // TODO: add requirements
---
+++ propersoft-core-verification/estimation-is-required.js
@@ -0,0 +1,30 @@
+/**
+ * This is a template for an on-change rule. This rule defines what
+ * happens when a change is applied to an issue.
+ *
+ * For details, read the Quick Start Guide:
+ * https://www.jetbrains.com/help/youtrack/standalone/2017.2/Quick-Start-Guide-Workflows-JS.html
+ */
+
+var entities = require('v1/entities');
+var workflow = require('v1/workflow');
+
+exports.rule = entities.Issue.onChange({
+  title: '必须填写评估工时',
+  action: function(ctx) {
+    var issue = ctx.issue;
+    var assigning = issue.isChanged('Assignee') && issue.Assignee !== null;
+    var verifing = issue.State.presentation === 'Verified';
+    if (assigning || verifing) {
+      var isBug = issue.Type.name.toLowerCase().indexOf('bug') > -1;
+      var period = issue.fields.Estimation;
+      var minutes = !period ? 0 : (period.getMinutes() + 60 * (period.getHours() + 8 * period.getDays()));
+      var hasEstimation = minutes > 0;
+      var requireEstimation = !isBug && !hasEstimation;
+      workflow.check(!requireEstimation, '非 bug 类型任务，指派或验证时必须填写评估工时！');
+    }
+  },
+  requirements: {
+    // TODO: add requirements
+  }
+});
