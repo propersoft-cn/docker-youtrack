592e00399837714fa7d1b96385f1247e8a276c3a
AlphaHinex <AlphaHinex@gmail.com>
2018-04-12 13:49:57 +0800

    Use 2018 format

--- propersoft-core-verification/auto-raise-priority.js
+++ propersoft-core-verification/auto-raise-priority.js
@@ -3,27 +3,28 @@
  * operations that are performed on a set schedule.
  *
  * For details, read the Quick Start Guide:
- * https://www.jetbrains.com/help/youtrack/standalone/2017.2/Quick-Start-Guide-Workflows-JS.html
+ * https://www.jetbrains.com/help/youtrack/standalone/2018.1/Quick-Start-Guide-Workflows-JS.html
  */
 
-var entities = require('v1/entities');
+var entities = require('@jetbrains/youtrack-scripting-api/entities');
 
 exports.rule = entities.Issue.onSchedule({
   title: '优先级自动升级',
   search: '#Unresolved #Major #Critical #Show-stopper -Unassigned',
   // From Tuesday to Saturday
-  // http://www.quartz-scheduler.org/documentation/quartz-2.x/tutorials/crontrigger.html
+  // For format details refer to http://www.quartz-scheduler.org/documentation/quartz-2.x/tutorials/crontrigger.html
   cron: '0 0 2 ? * TUE-SAT',
-//   cron: '0/5 * * * * ?',
-  action: function(ctx) {
+  guard: function(ctx) {
     var issue = ctx.issue;
-    
     if (issue.State.name === 'Done' || issue.State.name === "Verified") {
       console.log('Due to bug of state field, ignore ' + issue.id + ' which has "Done" or "Verified" state.');
-      return;
+      return false;
     }
-    
-    console.log('Start to raise priority of ' + issue.id);
+    return true;
+  },
+  action: function(ctx) {
+    var issue = ctx.issue;
+    // console.log('Start to raise priority of ' + issue.id);
     var critical = issue.project.findFieldByName('Priority').findValueByName('Critical');
     var ss = issue.project.findFieldByName('Priority').findValueByName('Show-stopper');
     
@@ -35,7 +36,7 @@ exports.rule = entities.Issue.onSchedule({
       var dd = issue.Delays || 0;
       issue.Delays = dd + 1;
     }
-    console.log('Complete raise priority of ' + issue.id);
+    // console.log('Complete raise priority of ' + issue.id);
   },
   requirements: {
     // TODO: add requirements
